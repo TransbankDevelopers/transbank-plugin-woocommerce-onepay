9901085d761f89f58214d2409fc282fcff856bbb
Rodrigo Ayala <rayalac@gmail.com>
2018-08-29 17:22:57 -0300

    Fix zip packaging to run in Linux

--- package.sh
+++ package.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-composer install
+composer install --no-dev
 
-sed -i '' 's/Version:           1.0.0/Version:           $TRAVIS_TAG/g' onepay.php
-sed -i '' "s/define( 'PLUGIN_NAME_VERSION', '1.0.0' );/define( 'PLUGIN_NAME_VERSION', '$TRAVIS_TAG' );/g" onepay.php
+sed -i "s/Version:           1.0.0/Version:           $TRAVIS_TAG/g" onepay.php
+sed -i "s/define( 'PLUGIN_NAME_VERSION', '1.0.0' );/define( 'PLUGIN_NAME_VERSION', '$TRAVIS_TAG' );/g" onepay.php
 
-zip -r9 "onepay-$TRAVIS_TAG.zip" . -x Dockerfile composer.json composer.lock docker-compose.yml init.sh package-lock.json docs *.git/\* .DS_Store* .editorconfig* .gitignore* .vscode/\* package.sh README.md
+zip -r9 "onepay-$TRAVIS_TAG.zip" . -x Dockerfile composer.json composer.lock docker-compose.yml init.sh package-lock.json docs/\* *.git/\* .DS_Store* .editorconfig* .gitignore* .vscode/\* package.sh README.md
